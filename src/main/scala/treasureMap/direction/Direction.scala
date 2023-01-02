ae4325b19e60bb45c39977c13e311274ba3dcaef
AP <malts-sur.09@icloud.com>
2023-01-02 17:09:23 +0100

    Typo corrected

--- src/main/scala/treasureMap/direction/Direction.scala
+++ src/main/scala/treasureMap/direction/Direction.scala
@@ -5,7 +5,7 @@ sealed case class Direction(value : String)
 case object Direction{
   object SOUTH extends Direction("S")
   object NORTH extends Direction("N")
-  object WEST extends Direction("W")
+  object WEST extends Direction("O")
   object EAST extends Direction("E")
 
   val values = Set(SOUTH, NORTH, WEST, EAST)
